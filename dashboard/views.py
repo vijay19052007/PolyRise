ca3fe0bf50b9f448231485dd008c82616d0fdda5
Vijay Bobade <vijaybobade94@gmail.com>
2025-08-12 01:55:26 +0530

    Update: My Dashboard all models and templates

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 # from django.contrib.auth.decorators import login_required  # Commented for now
+from .models import TodayILearned, DailyTechNews
+
 
 # @login_required
 def student_dashboard(request):
@@ -16,3 +18,14 @@ def admin_dashboard(request):
 # @login_required
 def home(request):
     return render(request, 'dashboard/home.html')
+
+
+def student_dashboard(request):
+    til = TodayILearned.objects.first()  # latest due to ordering by -date_added
+    tech_news = DailyTechNews.objects.first()
+
+    context = {
+        'til': til,
+        'tech_news': tech_news,
+    }
+    return render(request, 'dashboard/student_dashboard.html', context)
