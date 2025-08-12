9929ec7cc67f49d71c079ad0e2173945249310c6
mangesh-patil07 <shyamppatil07@example.com>
2025-08-12 18:53:23 +0530

    update :add database

--- support/models.py
+++ support/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class doubt(models.Model):
+    category=models.CharField()
+    email=models.EmailField()
+    message=models.TextField()
+    def __str__(self):
+        return self.category
--- support/views.py
+++ support/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse,HttpResponseRedirect
 from .models import *
+from django.contrib import messages
+
 # Create your views here.
 def support(request):
     return render(request,"support/support.html")
 
 def submit_doubt(request):
     if request.method == "POST":
-        ctg = request.POST.get('category')
-        msg = request.POST.get('message')
-        print(ctg,"= ",msg)
+        ctg = request.POST['category']
+        msg = request.POST['message']
+        email=request.POST['email']
+        question=doubt(category=ctg,message=msg,email=email)
+        question.save() 
+        messages.success(request, "Profile details updated.")
         return HttpResponseRedirect("/support")
     return redirect('support')
