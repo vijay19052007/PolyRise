793bd018538525df115f3d686c45defcbd6280a8
mangesh-patil07 <shyamppatil07@example.com>
2025-08-13 20:47:22 +0530

    add: planner timetable added

--- planner/models.py
+++ planner/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class timetable(models.Model):
+    topic=models.CharField(max_length=50)
+    date=models.DateField()
+    time=models.TimeField()
+    duration=models.IntegerField()
