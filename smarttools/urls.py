11ef730d95715e41918a1e0470503b3ef41cbe7b
mangesh-patil07 <shyamppatil07@example.com>
2025-08-27 11:04:40 +0530

    style are changes

--- smarttools/urls.py
+++ smarttools/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.tools, name='tools'),
     path('quiz/', views.quiz_start, name='quiz_start'),
-    path('unit-converter/', views.unit_converter, name='unit_converter'),
 ]
